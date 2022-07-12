2e8d2aa7f1c5d4b581a1bcddb96e592d20332af7
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-07-12 20:10:35 +0300

    v 1.0.5

--- src/gateway.tsx
+++ src/gateway.tsx
@@ -1,7 +1,7 @@
 import { IUser } from './dataTypes';
 
 const url = 'https://api.github.com';
-const token = 'token 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';
+const token = 'token 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';
 
 export const fetchUsers = async (name: string, page: number) => {
   try {
