db5d2ac6df2a0694d2285af424fcbdc7dab175ee
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-07-13 12:25:30 +0300

    testing env variable add env file 2

--- src/gateway.tsx
+++ src/gateway.tsx
@@ -3,16 +3,16 @@ import { IUser } from './dataTypes';
 const url = 'https://api.github.com';
 const key = process.env.REACT_APP_TOKEN;
 
+const headers = {
+  Authorization: `token ${key}`,
+  Accept: 'application/vnd.github+json',
+};
+
 export const fetchUsers = async (name: string, page: number) => {
   try {
     const response = await fetch(
       `${url}/search/users?q=${name}+in:login&per_page=15&page=${page}`,
-      {
-        headers: {
-          Authorization: `token ${key}`,
-          Accept: 'application/vnd.github+json',
-        },
-      },
+      { headers },
     );
 
     const responseData = await response.json();
@@ -25,11 +25,7 @@ export const fetchUsers = async (name: string, page: number) => {
 
 export const fetchUsersWithReposCount = async (users: IUser[]) => {
   const promisesArr = users.map(({ login }) => {
-    return fetch(`${url}/users/${login}`, {
-      headers: {
-        Authorization: `token ${key}`,
-      },
-    }).then(response => response.json());
+    return fetch(`${url}/users/${login}`, { headers }).then(response => response.json());
   });
 
   const newUsersList = await Promise.all(promisesArr);
@@ -39,11 +35,7 @@ export const fetchUsersWithReposCount = async (users: IUser[]) => {
 
 export const fetchUser = async (name: string | undefined) => {
   try {
-    const response = await fetch(`${url}/users/${name}`, {
-      headers: {
-        Authorization: `token ${key}`,
-      },
-    });
+    const response = await fetch(`${url}/users/${name}`, { headers });
     return response.json();
   } catch (error) {
     return error;
@@ -53,9 +45,7 @@ export const fetchUser = async (name: string | undefined) => {
 export const fetchUserRepos = async (name: string | undefined, page: number) => {
   try {
     const response = await fetch(`${url}/users/${name}/repos?per_page=10&page=${page}`, {
-      headers: {
-        Authorization: `token ${key}`,
-      },
+      headers,
     });
     return response.json();
   } catch (error) {
